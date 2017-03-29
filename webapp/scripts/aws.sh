ce377995abdc39e0e3e2400c91cd6ce022126bcd
tjfamodu <tjfamodu@eyemaginations.com>
2017-03-29 17:30:01 -0400

    chkconfig httpd on, removed awwslog script

--- webapp/scripts/aws.sh
+++ webapp/scripts/aws.sh
@@ -6,10 +6,11 @@ sudo yum install -y yum-utils
 sudo yum clean all
 sudo yum groupinstall 'Development Tools'
 sudo yum install -y httpd vim-enhanced bash-completion unzip wget tar bzip2 git
-sudo service httpd start && sudo chkconfig on
+sudo service httpd start 
 sudo yum install -y mysql mysql-server
 sudo yum clean all
 sudo service mysqld start && sudo chkconfig mysqld on
 sudo yum install -y php56w php56w-cli php56w-common php56w-mbstring, php56w-mysql, php56w-xml, php56w-gd
 wget https://phar.phpunit.de/phpunit-5.7.phar
 sudo service httpd restart
+sudo chkconfig httpd on
